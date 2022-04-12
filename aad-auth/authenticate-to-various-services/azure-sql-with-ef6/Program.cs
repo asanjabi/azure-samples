9e65faaa2658b8ae3c315f986ec15913670ff960
asanjabi <asanjabi@users.noreply.github.com>
2022-04-12 16:58:21 -0700

    Sample EF with default login

---
+++ aad-auth/authenticate-to-various-services/azure-sql-with-ef6/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using azure_sql_with_ef6;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+//Note the line <PackageReference Include="Microsoft.Data.SqlClient" Version="4.1.0" />
+//in the project file, this is required to support "Active Directory Default" authentication
+//EF6 by default uses version 2.1 which does not support AD Default auth, Version 3.0 or higher is required.
+string dbConnectionString = "Server=tcp:sql-4ogmj5slfs42a.database.windows.net,1433; Authentication=Active Directory Default; Database=sqldb-4ogmj5slfs42a;";
+
+Console.WriteLine("starting..");
+var options = new DbContextOptionsBuilder().UseSqlServer(dbConnectionString).Options;
+var ctx = new WidgetDbContext(options);
+
+ctx.Database.EnsureCreated();
+
+Console.WriteLine("Created");
+
---
+++ aad-auth/authenticate-to-various-services/azure-sql-with-ef6/WidgetDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace azure_sql_with_ef6;
+
+internal class WidgetDbContext: DbContext
+{
+    public WidgetDbContext(DbContextOptions options): base(options)
+    {
+
+    }
+
+    public DbSet<Widget> Widgets => Set<Widget>();
+
+}
